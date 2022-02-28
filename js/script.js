60892e4ba9ceb1bbf0aad9c1abd3d4f6578d09cb
sa-sabuj <sasabuj321@gmail.com>
2022-03-01 03:31:55 +0600

    get button click result

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@ const loadPhone = () => {
 }
 
 const displayPhone = allPhones => {
-    // console.log(allPhones)
+    console.log(allPhones)
     const phone = allPhones.data
     const phoneDetail = document.getElementById('phones');
     for (const phones of phone) {
-        const p = document.createElement('p');
-        p.innerText = `Brand: ${phones.brand}  Name: ${phones.phone_name}   Title: ${phones.slug}`
-        console.log(phones)
-        phoneDetail.appendChild(p)
+        const div = document.createElement('div')
+        div.classList.add('add-border')
+        div.innerHTML = `
+        <h2>Brand: ${phones.brand}</h2>
+        <h3>Brand:Name: ${phones.phone_name}</h3>
+        <p>Title: ${phones.slug}</p>
+        <img width = "100px" src="${phones.image}">
+        <br>
+        <button>Details</button>
+        `
+        // const h3 = document.createElement('h3');
+        // h3.innerText = `Brand: ${phones.brand}  Name: ${phones.phone_name}   Title: ${phones.slug}`
+        // console.log(phones)
+        // div.appendChild(h3)
+        phoneDetail.appendChild(div)
     }
 }
