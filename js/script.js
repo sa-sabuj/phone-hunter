7692f1676e8fa0d44286f13e5b5257cd701a0767
sa-sabuj <sasabuj321@gmail.com>
2022-03-01 22:46:50 +0600

    srarch detail added

--- js/script.js
+++ js/script.js
@@ -16,15 +16,19 @@ const displaySearchResult = data => {
         // console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col')
-        div.innerHTML = `<div onclick="allPhoneDetail('${phone.slug}')"  class="col">
+        div.innerHTML = `
+        
             <div class="card h-100">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${phone.brand}</h5>
                     <h5 class="card-title">${phone.phone_name}</h5>
                 </div>
+                <button onclick="allPhoneDetail('${phone.slug}')" class="btn btn-outline-secondary" type="button"
+            id="search-button">More Detail</button>
             </div>
-        </div>
+            
+        
     `
         searchResult.appendChild(div)
     });
@@ -32,28 +36,37 @@ const displaySearchResult = data => {
 }
 
 const allPhoneDetail = allPhoneId => {
-    // console.log(allPhoneId)
+    console.log(allPhoneId)
     const url = `https://openapi.programming-hero.com/api/phone/${allPhoneId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhoneDetail(data.data.mainFeatures))
+        .then(data => displayPhoneDetail(data.data))
+        console.log(url)
 }
 
+
 const displayPhoneDetail = phoneDetail => {
-    // console.log(phoneDetail)
+    console.log(phoneDetail)
     const singlePhoneDetail = document.getElementById('phone-details');
+    console.log(singlePhoneDetail)
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
 
-    <img src="${phoneDetail.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-    <div class="card-body">
-    <h5 class="card-title">${phoneDetail.brand}</h5>
-    <h5 class="card-title">${phoneDetail.phone_name}</h5>
+    
+                
+    <div class="card-body w-50  justify-content-center">
+    <img src="${phoneDetail.image}" class="mx-auto d-block" alt="...">
+        <h5 class="card-title">${phoneDetail.brand}</h5>
+        <h5 class="card-title">${phoneDetail.name}</h5>
+        <h5 class="card-title">${phoneDetail.mainFeatures.displaySize}</h5>
+        <h5 class="card-title">${phoneDetail.mainFeatures.memory}</h5>
+        <h5 class="card-title">${phoneDetail.slug}</h5>
+        <h5 class="card-title">${phoneDetail.releaseDate}</h5>
     </div>
     
     `;
+    console.log(div)
     singlePhoneDetail.appendChild(div)
 
 }
